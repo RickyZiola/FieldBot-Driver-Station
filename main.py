91feec4a2a54956abf0b6cb28cb39c0cf022784f
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2022-08-12 14:00:34 -0400

    Changed Theme and added input reading

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from asyncio.windows_events import NULL
 import tkinter as tk
-import sv_ttk
 from keyboard import *
-
+import XboxController
+from tkinter import ttk
 
 
 # Function declaration for bad scenarios
@@ -10,128 +11,39 @@ esc_was_pressed = False
 def emergency_exit():   # In the event of a bug in the driver station, you can hold down ESC to close it
     global esc_was_pressed
     if(esc_was_pressed and is_pressed("esc")):
-        root.destroy()
+        window.destroy()
     esc_was_pressed = is_pressed("esc")
-    root.after(1000, emergency_exit)
+    root.after(500, emergency_exit)
 
 def robot_kill():       # In the event of a robot software bug, press ENTER to kill all movement
     pass  # will be implemented with radio
 
+# Function declaration for input handling
+joystick = XboxController.XboxGamepad()
+def show_input():
+    lsx, lsy, rsx, rsy, a, b, x, y, lb, rb, lt, rt = joystick.read()
+    input_label.config(text=str(round(lsx, 2)) + "\n" + str(round(lsy, 2)) + "\n" + str(round(rsx, 2) ) + "\n" + str(round(rsy, 2)) + "\n" + str(round(a, 2) ) + "\n" + str(round(b, 2)) + "\n" + str(round(x, 2) ) + "\n" + str(round(y, 2)) + "\n" + str(round(lb, 2) ) + "\n" + str(round(rb, 2)) + "\n" + str(round(lt, 2) ) + "\n" + str(round(rt, 2)))
+    input_label.update_idletasks()
+    root.after(100, show_input)
 
-from inputs import get_gamepad
-import math
-import threading
+window = tk.Tk()
+window.tk.call("source", "Azure-ttk-theme/azure.tcl")
+window.tk.call("set_theme", "dark")
+root = ttk.Frame(window)
+root.pack(fill="both", expand=True)
 
-class XboxController(object):
-    MAX_TRIG_VAL = math.pow(2, 8)
-    MAX_JOY_VAL = math.pow(2, 15)
-
-    def __init__(self):
-
-        self.LeftJoystickY = 0
-        self.LeftJoystickX = 0
-        self.RightJoystickY = 0
-        self.RightJoystickX = 0
-        self.LeftTrigger = 0
-        self.RightTrigger = 0
-        self.LeftBumper = 0
-        self.RightBumper = 0
-        self.A = 0
-        self.X = 0
-        self.Y = 0
-        self.B = 0
-        self.LeftThumb = 0
-        self.RightThumb = 0
-        self.Back = 0
-        self.Start = 0
-        self.LeftDPad = 0
-        self.RightDPad = 0
-        self.UpDPad = 0
-        self.DownDPad = 0
-
-        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
-        self._monitor_thread.daemon = True
-        self._monitor_thread.start()
-
-
-    def read(self): # return the buttons/triggers that you care about in this method
-        x = self.LeftJoystickX
-        y = self.LeftJoystickY
-        x2 = self.RightJoystickX
-        y2 = self.RightJoystickY
-        a = self.A
-        b = self.B
-        x = self.X
-        y = self.Y
-        lb = self.LeftBumper
-        rb = self.RightBumper
-        lt = self.LeftTrigger
-        rt = self.RightTrigger
-        return [x, y, x2, y2, a, b, x, y, lb, rb, lt, rt]
-
-
-    def _monitor_controller(self):
-        while True:
-            events = get_gamepad()
-            for event in events:
-                if event.code == 'ABS_Y':
-                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_X':
-                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_RY':
-                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_RX':
-                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_Z':
-                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
-                elif event.code == 'ABS_RZ':
-                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
-                elif event.code == 'BTN_TL':
-                    self.LeftBumper = event.state
-                elif event.code == 'BTN_TR':
-                    self.RightBumper = event.state
-                elif event.code == 'BTN_SOUTH':
-                    self.A = event.state
-                elif event.code == 'BTN_NORTH':
-                    self.Y = event.state
-                elif event.code == 'BTN_WEST':
-                    self.X = event.state
-                elif event.code == 'BTN_EAST':
-                    self.B = event.state
-                elif event.code == 'BTN_THUMBL':
-                    self.LeftThumb = event.state
-                elif event.code == 'BTN_THUMBR':
-                    self.RightThumb = event.state
-                elif event.code == 'BTN_SELECT':
-                    self.Back = event.state
-                elif event.code == 'BTN_START':
-                    self.Start = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY1':
-                    self.LeftDPad = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY2':
-                    self.RightDPad = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY3':
-                    self.UpDPad = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY4':
-                    self.DownDPad = event.state
-
-
-
-
-if __name__ == '__main__':
-    joy = XboxController()
-    while True:
-        print(joy.read())
-
-
-# Function declaration for camera handling
-
-root = tk.Tk()
-sv_ttk.set_theme("dark")
-root.attributes('-fullscreen',True)
-label = tk.Label(root, text="Hello World")
+s = ttk.Style()
+s.configure('new.TFrame', background='#000000')
+test = ttk.Frame(root, style='new.TFrame')
+test.pack()
+input_label = ttk.Label(test, text='', background="#000000")
+input_label.pack()
+#sv_ttk.set_theme("dark")
+window.attributes('-fullscreen',True)
+label = ttk.Label(test, text="Hello World", background="#000000")
 label.pack()
-close_button = tk.Button(root, text="Close", command=root.destroy, font=("arial", 18))
+close_button = ttk.Button(root, text="Close", command=window.destroy)
 close_button.place(rely=1.0, relx=1.0, x=0, y=0, anchor='se')
-root.after(1000, emergency_exit)
-root.mainloop()
+window.after(500, emergency_exit)
+window.after(100, show_input)
+window.mainloop()
