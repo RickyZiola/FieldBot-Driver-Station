6b0dbcc7f79ce77ea84e49ed20b999f01e871ac5
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2022-08-12 20:07:47 -0400

    Removed Tests Added a confirmation window for closing  the DS

--- main.py
+++ main.py
@@ -3,7 +3,41 @@ import tkinter as tk
 from keyboard import *
 import XboxController
 from tkinter import ttk
+import numpy as math
 
+confWindow = NULL
+# Function to close Driver Station
+
+def exit_ds():
+    global confWindow
+
+    confWindow = tk.Tk()  # Create a conformation window
+
+    confWindow.title("Close Driver Station?")    # Title the window
+
+    confWindow.geometry("300x100")    # Resize and position the window
+    confWindow.eval('tk::PlaceWindow . center')
+
+    confWindow.tk.call("source", "Azure-ttk-theme/azure.tcl")  
+    confWindow.tk.call("set_theme", "dark")     # Add the theme to the window
+
+    confRoot = ttk.Frame(confWindow)   # Create a frame for the window
+    confRoot.pack(fill="both", expand=True)
+
+    label = ttk.Label(confRoot, text="Close Driver Station?", font=("arial", 20, "bold"))
+    label.pack()      # Create a prompt to close the DS
+
+    yesButton = ttk.Button(confRoot, text="Confirm", style="Accent.TButton", command=close)
+    yesButton.place(rely=1.0, relx=0.0, x=0, y=0, anchor='sw')      # Create a Confirm button
+
+    noButton = ttk.Button(confRoot, text="Cancel", style="TButton", command=confWindow.destroy)
+    noButton.place(rely=1.0, relx=1.0, x=0, y=0, anchor='se')       # Create a Cancel button
+
+    confWindow.mainloop()   # Run the window
+
+def close():    # Close the main a conformation windows
+    window.destroy()
+    confWindow.destroy()
 
 # Function declaration for bad scenarios
 
@@ -22,7 +56,7 @@ def robot_kill():       # In the event of a robot software bug, press ENTER to k
 joystick = XboxController.XboxGamepad()
 def show_input():
     lsx, lsy, rsx, rsy, a, b, x, y, lb, rb, lt, rt = joystick.read()
-    input_label.config(text=str(round(lsx, 2)) + "\n" + str(round(lsy, 2)) + "\n" + str(round(rsx, 2) ) + "\n" + str(round(rsy, 2)) + "\n" + str(round(a, 2) ) + "\n" + str(round(b, 2)) + "\n" + str(round(x, 2) ) + "\n" + str(round(y, 2)) + "\n" + str(round(lb, 2) ) + "\n" + str(round(rb, 2)) + "\n" + str(round(lt, 2) ) + "\n" + str(round(rt, 2)))
+    input_label.config(text=str(math.piecewise(round(lsx, 2), [abs(round(lsx, 2)) < 0.05, abs(round(lsx, 2)) >= 0.05], [0, round(lsx, 2)])) + "\n" + str(math.piecewise(round(lsy, 2), [abs(round(lsx, 2)) < 0.05, abs(round(lsy, 2)) >= 0.05], [0, round(lsy, 2)])) + "\n" + str(math.piecewise(round(rsx, 2), [abs(round(rsx, 2)) < 0.05, abs(round(rsx, 2)) >= 0.05], [0, round(rsx, 2)])) + "\n" + str(math.piecewise(round(rsy, 2), [abs(round(rsy, 2)) < 0.05, abs(round(rsy, 2)) >= 0.05], [0, round(rsy, 2)])) + "\n" + str(round(a, 2) ) + "\n" + str(round(b, 2)) + "\n" + str(round(x, 2) ) + "\n" + str(round(y, 2)) + "\n" + str(round(lb, 2) ) + "\n" + str(round(rb, 2)) + "\n" + str(round(lt, 2) ) + "\n" + str(round(rt, 2)))
     input_label.update_idletasks()
     root.after(100, show_input)
 
@@ -38,11 +72,11 @@ test = ttk.Frame(root, style='new.TFrame')
 test.pack()
 input_label = ttk.Label(test, text='', background="#000000")
 input_label.pack()
-#sv_ttk.set_theme("dark")
 window.attributes('-fullscreen',True)
 label = ttk.Label(test, text="Hello World", background="#000000")
 label.pack()
-close_button = ttk.Button(root, text="Close", command=window.destroy)
+s.configure("new.TButton", font=("arial", 20))
+close_button = ttk.Button(root, text="Close", command=exit_ds, style="new.TButton")
 close_button.place(rely=1.0, relx=1.0, x=0, y=0, anchor='se')
 window.after(500, emergency_exit)
 window.after(100, show_input)
